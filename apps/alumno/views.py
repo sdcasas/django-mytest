1c152d9a17aaada8b9731f15285ab37e0cdc064c
Sergio Casas <sdcasas@gmail.com>
2016-11-19 23:02:02 -0300

    define views to alumno app

--- apps/alumno/views.py
+++ apps/alumno/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
 
-# Create your views here.
+from django.views.generic import edit, ListView, DetailView
+
+from .models import *
+from .forms import *
+
+
+class AlumnoCreate(edit.CreateView):
+    model = Alumno
+    form_class = AlumnoForm
+    template_name = 'base/genericForm.html'
+
+
+class AlumnoUpdate(edit.UpdateView):
+    model = Alumno
+    form_class = AlumnoForm
+    template_name = 'base/genericForm.html'
+
+
+class AlumnoList(ListView):
+    model = Alumno
+    template_name = 'alumnoList.html'
+
+
+class AlumnoDetail(DetailView):
+    model = Alumno
+    template_name = 'alumnoDetail.html'
